5d1c9940b7ce97c67915fb8d651e606b2a1f6a08
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 21:12:19 +0300

    Rokas

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,45 +6,51 @@ import "./Main.css";
 
 const Main = ({activePunk}) => {
     return (
-        <div className="main">
-            <div className="mainContent">
+        <div className="main" style={{display: 'flex', flexDirection: 'column', paddingTop: '100px'}}>
+            <div className="mainContent" >
                 <div className="punkHighlight">
                     <div className="punkContainer">
                         <img src="https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg" className="selectedPunk" alt=""/>
                     </div>
                 </div>
 
-                <div className="punkDetails" style={{color: '#ffffff'}}>
-                    <div classNam="title">
-                        An Awesome Punk
-                    </div>
-                    <span className="itemNumber">•#1</span>
-                </div>
-                <div className="owner">
-                    <div className="ownerImageContainer">
-                        <img src="https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg" alt=""/>
-                    </div>
-                    <div className="ownerDetails">
-                        <div className="ownerNameAndHandle">
-                            <div className="">
-                                0x23DDc2606865a6feCe458E78D16Ffdf440785A88
+                <div  style={{display: 'flex', flexDirection: 'column'}}>
+                        <div className="punkDetails" style={{color: '#ffffff', flex: 1,}}>
+                            <div className="title" style={{display: 'flex', maxWidth: '20px'}}>
+                                An Awesome Punk
                             </div>
-                            <div className="ownerHandle">
-                                @rookasrudzianskas
-                            </div>
-                            <div className="ownerLink">
-                                <img src={instagramLogo} alt=""/>
-                            </div>
-                            <div className="ownerLink">
-                                <img src={twitterLogo} alt=""/>
-                            </div>
-                            <div className="ownerLink">
-                                <img src={moreIcon} alt=""/>
+                        </div>
+                    <div className="owner" style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+                        <span className="itemNumber">•#1</span>
+                        <div className="ownerImageContainer">
+                            <img src="https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg" alt=""/>
+                        </div>
+                        <div className="ownerDetails" style={{display: 'flex', flexDirection: 'row', alignItems: 'center', flex: 1}}>
+                            <div className="ownerNameAndHandle" style={{flex: 1, display: 'flex'}}>
+                                <div style={{display: 'flex', alignItems: 'center'}}>
+                                    <div className="">
+                                        0x23DDc2606865a6feCe458E78D16Ffdf440785A88
+                                    </div>
+                                    <div className="ownerHandle">
+                                        @rookasrudzianskas
+                                    </div>
+                                </div>
+                                <div className="ownerLink">
+                                    <img src={instagramLogo} alt=""/>
+                                </div>
+                                <div className="ownerLink">
+                                    <img src={twitterLogo} alt=""/>
+                                </div>
+                                <div className="ownerLink">
+                                    <img src={moreIcon} alt=""/>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
+
         </div>
     );
 };
