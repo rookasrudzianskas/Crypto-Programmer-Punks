e7bc9807b7ebf23d58f6eede4653d7c809cca02d
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 16:29:43 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ function App() {
   return (
     <div className="app">
       <Header />
-        <CollectionCard id={0} name={'Bandana Punk'} traits={[{'value': 7}]} image={'https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg'} />
+        <CollectionCard
+            id={0}
+            name={'Bandana Punk'}
+            traits={[{'value': 7}]}
+            image={'https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg'}
+        />
 
     </div>
   );
