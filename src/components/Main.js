918eb076ed1725893406b7a04c7e63227e59eb78
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 17:37:06 +0300

    Rokas

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Main = () => {
     return (
         <div className="main">
-
+            <div className="mainContent">
+                <div className="punkHighlight">
+                    <div className="punkContainer">
+                        <img src={activePunk.image_original_url} className="selectedPunk" alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
