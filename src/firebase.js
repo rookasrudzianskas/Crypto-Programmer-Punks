6121eaf47bba5436b6f910ec5e0b1f6f2d90433e
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 15:52:02 +0300

    Rokas

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDel0CwoFyl9L-2ZLerUoDg7vGAmG6-d9c",
+    authDomain: "rookas-nft.firebaseapp.com",
+    projectId: "rookas-nft",
+    storageBucket: "rookas-nft.appspot.com",
+    messagingSenderId: "405630022204",
+    appId: "1:405630022204:web:0af68bd783942394ec0939",
+    measurementId: "G-JG95BNS7DS"
+};
