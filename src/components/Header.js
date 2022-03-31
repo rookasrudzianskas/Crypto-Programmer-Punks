0fa02ed111a7d662ff91c50c438a99f1d8a44db7
Rokas <rookas.rudzenskas@gmail.com>
2022-03-31 15:49:01 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import punkLogo from '../assets/header/cryptopunk-logo.png';
 import searchIcon from '../assets/header/search.png';
+import themeSwitchIcon from '../assets/header/theme-switch.png';
 
 const Header = () => {
     return (
@@ -18,7 +19,15 @@ const Header = () => {
             </div>
 
             <div className="headerItems">
+                <p>Drops</p>
+                <p>Marketplace</p>
+                <p>Create</p>
+            </div>
 
+            <div className="headerActions">
+                <div className="themeSwitchContainer">
+                    <img src={themeSwitchIcon} alt=""/>
+                </div>
             </div>
         </div>
     );
